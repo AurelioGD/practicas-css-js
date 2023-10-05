7b8f4c82354d5f9c3c5f676d3f12da1b618999da
Aurelio Gonzalez <agonzalez@sisu.mx>
2023-10-04 22:09:35 -0700

    Avance menu, aun tiene bichos

--- responsive-shoe-website-main/assets/js/main.js
+++ responsive-shoe-website-main/assets/js/main.js
@@ -1,5 +1,16 @@
 /*=============== SHOW MENU ===============*/
+const SHOW_MENU_CLASS = "show-menu"
 
+const menuIconElement = document.querySelector("#menu-icon")
+const menuCloseIconElement = document.querySelector(".menu-close-icon")
+const menuContainerElement = document.querySelector(".menu_anchors")
+menuIconElement.addEventListener("click", () => {
+    menuContainerElement.classList.add(SHOW_MENU_CLASS)
+})
+
+menuCloseIconElement.addEventListener("click", () => {
+    menuContainerElement.classList.remove(SHOW_MENU_CLASS)
+})
 
 /*=============== REMOVE MENU MOBILE ===============*/
 
