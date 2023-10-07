ab0e9224e6ca9faf21ddb99f9c4f13d9bd3a397e
Aurelio Gonzalez <agonzalez@sisu.mx>
2023-10-07 00:10:24 -0700

    Slider y avance waca responsive

--- responsive-shoe-website-main/assets/js/main.js
+++ responsive-shoe-website-main/assets/js/main.js
@@ -12,6 +12,16 @@ menuCloseIconElement.addEventListener("click", () => {
     menuContainerElement.classList.remove(SHOW_MENU_CLASS)
 })
 
+const swiper = new Swiper('.swiper', {
+    speed: 800,
+    spaceBetween: 100,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true
+    }
+});
+
+const swiperContainer = document.querySelector('.swiper').swiper;
 /*=============== REMOVE MENU MOBILE ===============*/
 
 
